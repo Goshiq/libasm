70a5c7c1db0c596cfb863c9e41463c5e95f34259
Jerald Mogo <jmogo@et-j5.kzn.21-school.ru>
2021-04-16 09:21:23 +0300

    rewrote main

--- includes/libasm.h
+++ includes/libasm.h
@@ -8,6 +8,9 @@
 
 size_t	ft_read(int fd, void *buf, size_t nbytes);
 size_t	ft_strlen(const char *str);
+int		*ft_strcmp(char *s1, char *s2);
+char	*ft_strcpy(char *dst, char *src);
+char	*ft_strdup(char *str);
 size_t	ft_write(int fd, void *buf, size_t nbytes);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jmogo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 18:13:51 by jmogo             #+#    #+#             */
-/*   Updated: 2021/04/15 22:00:45 by jmogo            ###   ########.fr       */
+/*   Updated: 2021/04/16 09:20:40 by jmogo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 
-	char	*str = "It is a test string";
 	char	*buf = malloc(1024);
 
-	printf("String is: %s\nOS strlen: %lu\nMy strlen: %lu\n\n", str, strlen(str), ft_strlen(str));
-	printf("OS write: \n");
-	write(1, str, 19);
-	printf("\nMy write: \n");
-	ft_write(1, str, 19);
-	printf("\n\n");
 	printf("Checking read: \n");
 	ft_read(0, buf, 32);
-	printf("Result is:\n%s\n\n", buf);
+	printf("\nResult is:\n%s\n\n", buf);
+	printf("OS strlen: %lu\nMy strlen: %lu\n\n", strlen(buf), ft_strlen(buf));
+	printf("OS write: \n");
+	write(1, buf, ft_strlen(buf));
+	printf("\nMy write: \n");
+	ft_write(1, buf, ft_strlen(buf));
+	printf("\n\n");
+	free(buf);
 }
